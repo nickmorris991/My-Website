46a1588be6266530befcb8f7136010b10a687d38
Nicholas Morris <nickmorris25@outlook.com>
2020-05-12 00:31:00 -0400

    restruct index html & rough first version of contact icons finished

---
+++ static/js/index.redirects.js
@@ -0,0 +1,11 @@
+function emailRedirect() {
+    location.href = 'mailto:morris.1489@osu.edu';
+}
+
+function githubRedirect() {
+    location.href = 'https://github.com/nickmorris991';
+}
+
+function linkedinRedirect() {
+    location.href = 'https://www.linkedin.com/in/nicholas-jacob-morris/';
+}
