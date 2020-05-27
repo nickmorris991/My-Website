bc0a40c84cf3d2239317283ca6431e08fb979cda
Nicholas Morris <nickmorris25@outlook.com>
2020-05-27 01:25:16 -0400

    added basic structure change for trees

--- static/js/index/background.animation.js
+++ static/js/index/background.animation.js
@@ -3,9 +3,15 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 
-let animArray;
+// size of each node on a tree
+const nodeSize = 13;
 
-function Particle(x, y, xDirection, yDirection, size, color) {
+// each binary tree representation in this array.
+let binaryTreeArray; 
+
+
+// 'size' is the number of nodes on the tree, not a node's size.
+function Tree(x, y, xDirection, yDirection, size, color) {
     this.x = x;
     this.y = y;
     this.xDirection = xDirection;
@@ -15,15 +21,27 @@ function Particle(x, y, xDirection, yDirection, size, color) {
 }
 
 
-Particle.prototype.draw = function() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
+Tree.prototype.draw = function() {
+    // TODO: loop for this.size (number of nodes in the tree) creating the appropriate coordinates
+    var coords = [ [this.x,this.y], [this.x + 100,this.y + 100]];
     ctx.fillStyle = this.color;
-    ctx.fill();
+    ctx.strokeStyle = this.color;
+
+    for (var i=0; i < coords.length; i++) {
+        // draw a node of the tree and line to the next node (if there is one)
+        ctx.beginPath();
+        ctx.arc(coords[i][0], coords[i][1], nodeSize, 0, Math.PI * 2, false);
+        if(i+1 < coords.length) {
+            ctx.moveTo(coords[i][0], coords[i][1]);
+            ctx.lineTo(coords[i+1][0], coords[i+1][1]);
+            ctx.stroke();
+        }
+        ctx.fill();
+    }
 }
 
 
-Particle.prototype.update = function() {
+Tree.prototype.update = function() {
     if (this.x + this.size > canvas.width || this.x - this.size < 0) {
         this.xDirection = -this.xDirection;
     }
@@ -31,23 +49,23 @@ Particle.prototype.update = function() {
         this.yDirection = -this.yDirection;
     }
 
-    this.x += this.xDirection;
-    this.y += this.yDirection;
+    // this.x += this.xDirection;
+    // this.y += this.yDirection;
 
     this.draw();
 }
 
 
-function init() {
-    animArray = [];
-    for (let i=0; i < 100; i++) {
+function createTrees() {
+    binaryTreeArray = [];
+    for (let i=0; i < 5; i++) {
         let color = 'white';
-        let size = Math.random() * 20;
+        let size = Math.random() * 10;
         let x = Math.random() * (innerWidth - size * 2);
         let y = Math.random() * (innerHeight - size * 2);
         let xDirection = (Math.random() * .4) - .2;
         let yDirection = (Math.random() * .4) - .2;
-        animArray.push(new Particle(x, y, xDirection, yDirection, size, color));
+        binaryTreeArray.push(new Tree(x, y, xDirection, yDirection, size, color));
     }
 
 }
@@ -57,8 +75,8 @@ function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0,0, innerWidth, innerHeight); 
 
-    for (let i=0; i < animArray.length; i++) {
-        animArray[i].update();
+    for (let i=0; i < binaryTreeArray.length; i++) {
+        binaryTreeArray[i].update();
     }
 }
 
@@ -67,12 +85,12 @@ window.addEventListener('resize',
     function() {
         canvas.width = innerWidth;
         canvas.height = innerHeight;
-        init();
+        createTrees();
     }
 )
 
 
-init();
+createTrees();
 animate();
 
 
