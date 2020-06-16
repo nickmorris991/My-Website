e2182e597f20f4e56b2cc450cb7969139faee539
Andrew Smith <andrewts129@gmail.com>
2020-06-15 21:01:51 -0400

    better path construction

--- models/processProjects.py
+++ models/processProjects.py
@@ -1,9 +1,9 @@
 import json
-import os.path as path
+from os import path
 
 
 def getProjects():
-    with open("models" + path.sep + "projects.json", "r") as projects_data:
+    with open(path.join("models", "projects.json"), "r") as projects_data:
         proj_data = json.load(projects_data)["projects"]
 
     return proj_data
