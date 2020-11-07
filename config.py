dc5ed977a15fdbbc445db662f3eb41ebdb5321c6
root <root@ip-172-31-5-253.us-east-2.compute.internal>
2020-11-07 04:40:07 +0000

    updated gunicorn prod config, tested in prod.

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
-daemon = True
+import multiprocessing
+
+daemon = False
+workers = multiprocessing.cpu_count() * 2 + 1
+threads = multiprocessing.cpu_count() * 2 + 1
 bind = '0.0.0.0:5000'
 accesslog = "-"
+errorlog = "-"
