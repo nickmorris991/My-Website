2f9428be8b5cc2ca542185011a5a6dd9d46f46d2
nickmorris991 <nickmorris25@outlook.com>
2020-11-07 15:19:46 -0600

    more organized logging

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import multiprocessing
+import gunicorn_color
 
+# app config
 daemon = False
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2 + 1
 bind = '0.0.0.0:5000'
+
+# log config
 accesslog = "-"
 errorlog = "-"
+logger_class=gunicorn_color.Logger
+access_log_format="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s"
+loglevel="INFO"
