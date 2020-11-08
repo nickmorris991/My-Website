2eeb5011c256c8ea11f2d61b01296a96920c0cce
nickmorris991 <nickmorris25@outlook.com>
2020-11-08 16:39:07 -0600

    style updates for deployment

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ The source code for my website. Built with flask, bootstrap, & docker.
 
 
 #### potential features / todo's
+- validate service hook in deployment
 - light/dark mode switch
 - rotating trees
 - easter eggs coming soon
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-echo "updating instance with CodeDeploy"
+echo "updating instance & codebase with CodeDeploy"
 cd /home/nickmorr-dot-is/
 
 # stop/prune existing containers & pull latest build
 docker stop $(docker container ls -aq) && yes | docker system prune
 docker pull nickmorris991/nickmorr-dot-is:latest
 
-# run latest image with updated code from CodeDeploy
-docker run -d -t -p 80:5000 nickmorris991/nickmorr-dot-is
+# run latest image & final prune of all dangling data
+docker run -d -t -p 80:5000 --name personal-site nickmorris991/nickmorr-dot-is 
+yes | docker system prune
